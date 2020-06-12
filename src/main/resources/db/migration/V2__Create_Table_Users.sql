1d90e82cd3dc563c1d1ba8dc04805bea6ac769e4
Joshua Zenn <wordman05@gmail.com>
2020-06-11 21:47:10 -0400

    Fixed SQL syntax error

--- src/main/resources/db/migration/V2__Create_Table_Users.sql
+++ src/main/resources/db/migration/V2__Create_Table_Users.sql
@@ -1,15 +1,15 @@
 -- Script courtesy of https://www.roytuts.com/spring-boot-security-form-based-jdbc-authentication-using-userdetailsservice/
 
 -- Creates a table to store all registered users with their hashed password.
-CREATE TABLE user (
-    user_name varchar(255) NOT NULL PRIMARY KEY,
-    user_password varchar(255) NOT NULL,
-    user_email varchar(255) NOT NULL
+CREATE TABLE IF NOT EXISTS user (
+    user_name VARCHAR(255) NOT NULL PRIMARY KEY,
+    user_password VARCHAR(255) NOT NULL,
+    user_email VARCHAR(255) NOT NULL
 );
 
 -- Creates a table of roles, describing the privileges of the authenticated user.
-CREATE TABLE user_role (
-    user_name varchar(255) NOT NULL,
-    user_role varchar(15) NOT NULL,
-    FOREIGN KEY (user_name) REFERENCES user (user_name)
+CREATE TABLE IF NOT EXISTS user_role (
+    user_name VARCHAR(255) NOT NULL,
+    user_role VARCHAR(15) NOT NULL,
+    FOREIGN KEY (user_name) REFERENCES user(user_name)
 );
